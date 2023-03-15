b76f396f24080e2f0ed8797b05848a45a1eedd9a
Fahimphero <irfanulislamfahim@gmail.com>
2023-03-16 00:00:18 +0600

    last-commit-(from-added)

--- src/Pages/Bonus/Form/Form.js
+++ src/Pages/Bonus/Form/Form.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { toast } from 'react-toastify';
+import emailjs from '@emailjs/browser';
 import image from '../../../Images/Contact-Us.png'
 import './Form.css'
 
 const Form = () => {
+
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_hchpc4e', 'template_76vnvt6', form.current, 'Ya84B7fUSwl1YeCWA')
+            .then((result) => {
+                console.log(result.text);
+                toast.success('Thank you!! Successfully received your email. XTreme Tech will reply very soon')
+                e.target.reset();
+            },
+                (error) => {
+                    console.log(error.text);
+                });
+    };
+
+
+
     return (
         <div className='container'>
             <div className='text-center mb-4'>
@@ -14,18 +34,18 @@ const Form = () => {
                     <img className='img-fluid' src={image} alt="" />
                 </div>
                 <div className="col-12 col-lg-6 ">
-                    <form className='w-75 mx-auto p-3 rounded-3' style={{ border: '7px solid royalblue' }}>
+                    <form ref={form} onSubmit={sendEmail} className='w-75 mx-auto p-3 rounded-3' style={{ border: '7px solid royalblue' }}>
                         <div className="mb-3 mt-3 ">
                             <label htmlFor="exampleInputName1" className="form-label text-light">Your Name</label>
-                            <input type="text" className="form-control" id="exampleInputName1" aria-describedby="nameHelp" ></input>
+                            <input type="text" name="user_name" className="form-control" id="exampleInputName1" aria-describedby="nameHelp" required></input>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label text-light">Your Email</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                            <input type="email" name="user_email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required></input>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="exampleFormControlTextarea1" className="form-label text-light">Your Message</label>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea className="form-control" name="message" id="exampleFormControlTextarea1" rows="3" required></textarea>
                         </div>
                         <button type="submit" className="btn button text-light w-100 mb-4"><span className='fs-5'>Submit</span></button>
                     </form>
